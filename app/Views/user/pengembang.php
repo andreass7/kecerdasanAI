6f33d7e613a7711f0cfb21200ade7be9e20ace39
Ghufron03 <62162384+Ghufron03@users.noreply.github.com>
2024-06-04 00:57:30 +0700

    aaaaa

--- app/Views/user/pengembang.php
+++ app/Views/user/pengembang.php
@@ -1,6 +1,93 @@
 <?php $this->extend('layout/layout') ?>
 <?php $this->section('content') ?>
 
-pengembang
+<section id="team" class="team section-bg">
+    <div class="container" data-aos="fade-up">
+        <div class="section-title">
+            <h2>Pengembang</h2>
+            <p>
+                Website ini ditujukan untuk memnuhi tugas project mata kuliah kecerdasan buatan dengan dosen pengambu Ibu Yumarlin
+            </p>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                <div class="member">
+                    <div class="member-img">
+                        <img src="assets/img/team/1.jpg" class="img-fluid" alt="" />
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Yumarlin.Mz</h4>
+                        <span>Dosen Pengampu</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
+                <div class="member">
+                    <div class="member-img">
+                        <img src="assets/img/team/11.jpg" class="img-fluid" alt="" />
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Andreas Adi Prasetyo</h4>
+                        <span>213300</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="200">
+                <div class="member">
+                    <div class="member-img">
+                        <img src="assets/img/team/13.jpg" class="img-fluid" alt="" />
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Budi Anggoro Sajiwo Putro</h4>
+                        <span>213300</span>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
+                <div class="member">
+                    <div class="member-img">
+                        <img src="assets/img/team/12.jpg" class="img-fluid" alt="" />
+                        <div class="social">
+                            <a href=""><i class="bi bi-twitter"></i></a>
+                            <a href=""><i class="bi bi-facebook"></i></a>
+                            <a href=""><i class="bi bi-instagram"></i></a>
+                            <a href=""><i class="bi bi-linkedin"></i></a>
+                        </div>
+                    </div>
+                    <div class="member-info">
+                        <h4>Moch. Ghufron Amin As Ari</h4>
+                        <span>21330049</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End Team Section -->
+
 
 <?php $this->endSection() ?>
