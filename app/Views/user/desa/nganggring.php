15d0916725bfb98750ebc1a86c96c3519291c0c9
Ghufron03 <62162384+Ghufron03@users.noreply.github.com>
2024-06-04 10:09:09 +0700

    a

--- app/Views/user/desa/nganggring.php
+++ app/Views/user/desa/nganggring.php
@@ -1,6 +1,15 @@
 <?php $this->extend('layout/layout') ?>
 <?php $this->section('content') ?>
 <section>
-    Nganggring
+    <div class="container">
+        <div class="nganggring">
+            <h1 class="text-center">Desa Wisata Nganggring</h1>
+            <img src="assets/img/desa/nganggring.jpg" class="img-fluid" alt="" />
+            <p class="text-center">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas molestiae dignissimos rem tenetur eaque neque est illum ad accusantium sit, ipsum enim, sunt reprehenderit autem iusto, reiciendis nisi cum libero.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit assumenda consequatur, rerum corrupti praesentium eveniet dignissimos repellendus maiores voluptatum quia odio quibusdam, culpa libero nam ducimus sint laborum recusandae fugit eaque voluptates. Voluptate culpa blanditiis dolore ratione architecto quisquam repellendus error omnis, autem, saepe, est eaque sequi ex id sapiente.</p>
+
+        </div>
+    </div>
+
 </section>
 <?php $this->endSection() ?>
